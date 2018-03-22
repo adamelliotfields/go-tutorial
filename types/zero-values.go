a79acd88e69ac84f4b6b0b94a97add7714f85ba5
Adam Fields <afields@frtservices.com>
2018-03-22 00:17:06 -0400

    Added types.

---
+++ types/zero-values.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+var (
+	// 0
+	i int
+	// 0
+	f float64
+	// false
+	b bool
+	// ""
+	s string
+)
+
+func main() {
+	fmt.Printf("%v %v %v %q\n", i, f, b, s)
+}
