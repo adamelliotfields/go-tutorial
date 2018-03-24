6bb0dda9b0bbd4f3cf24da27ffca22575c29d052
Adam Fields <afields@frtservices.com>
2018-03-24 16:57:21 -0400

    Updated packages.

--- packages/main.go
+++ packages/main.go
@@ -1,10 +1,13 @@
-// https://tour.golang.org/basics/1
-
 // Programs start running in package main.
 package main
 
+// You can group imports into a "factored" import statement.
+// You can also write multiple import statements.
+// import "fmt"
+// import "math"
 import (
 	"fmt"
+	"math"
 	"math/rand"
 	"time"
 )
@@ -12,11 +15,17 @@ import (
 func main() {
 	// Seed the random number generator with the Unix time to ensure a different number every time.
 	rand.Seed(time.Now().Unix())
+
 	// The := syntax is shorthand for declaring and initializing a variable.
 	// Go will infer the type of initialized variables.
 	// This syntax can only be used inside a function.
 	// var randIntn int = rand.Intn(100)
 	randIntn := rand.Intn(100)
 
-	fmt.Println("My favorite number is", randIntn)
+	// A name is exported if it begins with a capital letter, like Pi.
+	pi := math.Pi
+
+	fmt.Printf("My favorite number is %d\n", randIntn)
+	fmt.Printf("Pi is %g\n", pi)
+	fmt.Printf("The square root of 7 is %g\n", math.Sqrt(7))
 }
